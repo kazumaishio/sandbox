c30423ef3654f008e68e161b3c64a86e1367df71
Kazuma Ishio <kazumaishio@gmail.com>
2016-06-28 14:36:29 +0200

    initial commit
    just hello world

---
+++ aaa.cpp
@@ -0,0 +1,17 @@
+//
+//  aaa.cpp
+//  
+//
+//  Created by 石尾 一馬 on 28/06/16.
+//
+//
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+int main(int argc, char** argv)
+{
+  cout<<"Hello world!"<<endl;
+  return 0;
+}
